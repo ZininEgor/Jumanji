c81014a2c9962c7f336236e2bd6aac14360f6e5b
ZininEgor <zininegor00@gmail.com>
2020-09-04 15:26:30 +0300

    Initial commit2

--- Vacancies_stepik/settings.py
+++ Vacancies_stepik/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'Vacancies_stepik.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'demo_1',
-#         'USER': 'ZininEgor',
-#         'PASSWORD': 'E2105gor',
-#         'HOST': 'database-1.ckeqpre8lkjr.us-east-2.rds.amazonaws.com',
-#         'PORT': '5432',
-#
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo_1',
+        'USER': 'ZininEgor',
+        'PASSWORD': 'E2105gor',
+        'HOST': 'database-1.ckeqpre8lkjr.us-east-2.rds.amazonaws.com',
+        'PORT': '5432',
+
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # import dj_database_url
 # db_from_env = dj_database_url.config(conn_max_age=600)

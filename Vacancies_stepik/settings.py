2df6f7b90956dd6777eb0a8fa55162f984038548
ZininEgor <zininegor00@gmail.com>
2020-09-04 15:17:52 +0300

    Initial commit2

--- Vacancies_stepik/settings.py
+++ Vacancies_stepik/settings.py
@@ -139,6 +139,8 @@ MEDIA_URL = '/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 

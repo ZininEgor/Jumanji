7f893f7870524658beed132829338f0a45f6bc35
ZininEgor <zininegor00@gmail.com>
2020-09-06 11:52:30 +0300

    Initial commit2

--- Vacancies_stepik/settings.py
+++ Vacancies_stepik/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'Vacancies_stepik.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+#
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
